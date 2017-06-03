0940aba4f21ea0175fb20cc6ebc66b782f0e6f9f
Mike <m_ran66@hotmail.com>
2017-06-02 19:42:29 -0700

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+  if(err){
+    return console.log('Unable to connect to db server');
+  }
+  console.log('Connected to MongoDB server');
+
+  //findOneAndUpdate - need to checkout update operators in mongodb
+  // db.collection('Todos').findOneAndUpdate({
+  //   text:'Win the lottery'
+  // },{
+  //   $set:{
+  //     completed:true
+  //   }
+  // },{
+  //   returnOriginal: false
+  // }).then((result)=>{
+  //   console.log(result);
+  // })
+
+  //Todos set name to mike and increment age by one
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID("5932040125941ee59df8af59")
+  },{
+    $set:{
+      name:"Mike"
+    },
+    $inc:{
+      age:2
+    }
+  },{
+    returnOriginal: false
+  }).then((result)=>{
+    console.log(result);
+  })
+
+
+  //db.close();
+});
